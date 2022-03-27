9106055c1f4d2f6f309dedb99f1fd32aaf119ada
ungalexy <ungalexy@gmail.com>
2022-03-27 19:50:09 +0300

    comment

--- contracts/CoffeeToken.sol
+++ contracts/CoffeeToken.sol
@@ -3,6 +3,9 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
+/**
+ * ERC-20 boilerplate
+ */
 contract CoffeeToken is ERC20 {
     constructor(uint256 initialSupply) ERC20("Coffee Token", "COFF") {
         _mint(msg.sender, initialSupply);
